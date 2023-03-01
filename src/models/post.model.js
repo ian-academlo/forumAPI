413b15027c912b22ae3f4bad2040eaa84370e802
Ian Rosas <ian.rosas@academlo.com>
2023-02-28 21:33:01 -0600

    posts fixed

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,29 +1,37 @@
 const { DataTypes } = require("sequelize");
 const db = require("../utils/database");
 
-const Posts = db.define("posts", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Posts = db.define(
+  "posts",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    categoryId: {
+      // js usamos camelCase
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: "category_id", // SQL snake_case
+    },
   },
-  title: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  author: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  categoryId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    field: "category_id",
-  },
-});
+  {
+    timestamps: true,
+    updatedAt: false,
+  }
+);
 
 module.exports = Posts;

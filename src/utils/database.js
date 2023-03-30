ab60740504e68f43aff0f9c06b112d1d899e898e
Ian Rosas <ian.rosas@academlo.com>
2023-03-30 17:44:14 -0600

    ssl added in sequelize configuration

--- src/utils/database.js
+++ src/utils/database.js
@@ -9,6 +9,7 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
   logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 
 module.exports = db;
